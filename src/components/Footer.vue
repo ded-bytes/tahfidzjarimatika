61385541a7331e5643ebdd89ae535a2976689447
ded-bytes <49288889+ded-bytes@users.noreply.github.com>
2024-04-25 16:34:16 +0800

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,72 @@
+<template>
+    <footer class="footer bg-dark text-white p-3">
+        <div class="container">
+            <div class="row pt-5">
+                <div class="col-md-2 text-center">
+                    <img src="../../public/img/icons/favicon-32x32.png" class="justify-content-center logo" alt="">
+                </div>
+                <div class="col-md-6">
+                    <h5>Tahfidz Jarimatika</h5>
+                    <p class="text-muted">
+                        Sebuah Aplikasi Oleh Rizky Mutmainnah yang digunakan untuk pemudahan Tahfidz dengan metode jarimatika.
+                    </p>
+                </div>
+                <div class="col-md-2">
+                    <h5>Social Media</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="https://www.instagram.com/rizki_mthmainnahamr/" class="text-decoration-none text-white"
+                                target="_blank">Researcher</a>
+                        </li>
+                        <li><a href="https://www.instagram.com/pammipams_/"
+                                class="text-decoration-none text-white" target="_blank">Developer</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <h5>Sources</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="https://quran.kemenag.go.id/"
+                                class="text-decoration-none text-white" target="_blank">Kementrian Agama</a></li>
+                        <li><a href="https://equran.id/"
+                                class="text-decoration-none text-white" target="_blank">E-Quran.ID</a></li>        
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-12 justify-content-center text-center">
+                    <span class="">
+                        Made for Rizky Mutmainnah . All Rights & Exclusivity Included 
+                    </span>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterVue',
+    components: {},
+}
+</script>
+
+<style scoped>
+.mt-9 {
+    margin-top: 9rem;
+}
+
+img.logo {
+    width: 75%;
+}
+
+@media only screen and (max-width: 768px) {
+    img.logo {
+        width: 50%;
+    }
+
+    .footer {
+        text-align: center;
+    }
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,46 @@
+<template>
+    <main class="container mt-custom ">
+        <div class="home bg-success text-white rounded text-center shadow mb-4">
+            <h1>BISMILLAHIRROHMANIRROHIIM</h1>
+            <p class="lead content">
+                Aplikasi ini diperuntukkan untuk kemudahan menghafal Al-Quran dengan metode JARIMATIKA. Aplikasi ini disusun dengan sumber ini dari
+                <a href="https://quran.kemenag.go.id/" class="text-decoration-none text-warning"
+                    target="_blank">Kementrian Agama Republik Indonesia
+                </a> dengan murottal
+                <strong>Misyari Rasyid Al-'Afasi</strong>.
+                untuk memenuhi syarat kualifikasi dan media penelitian utama menuju gelar Magister
+            </p>
+            <!-- <div>
+                <router-link :to="{ name: 'home' }" class="btn btn-warning m-1">Surah</router-link>
+                <router-link :to="{ name: 'juz' }" class="btn btn-warning m-1">Juz</router-link>
+            </div> -->
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'HomeVue',
+    components: {},
+}
+</script>
+
+<style scoped>
+.mt-custom {
+    margin-top: 95px;
+}
+
+.home {
+    padding: 30px;
+}
+
+@media screen and (max-width: 768px) {
+    .home {
+        padding: 15px;
+    }
+
+    .content {
+        font-size: 15px;
+    }
+}
+</style>
